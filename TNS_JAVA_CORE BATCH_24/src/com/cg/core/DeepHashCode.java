9a6efd145059ac236c4ef88302815bcbbecf1f08
shwet <shwet@Shweta>
2022-09-04 15:06:01 +0530

    Deep Hash Code

---
+++ TNS_JAVA_CORE BATCH_24/src/com/cg/core/DeepHashCode.java
@@ -0,0 +1,16 @@
+package com.cg.core;
+
+import java.util.Arrays;
+
+public class DeepHashCode {
+
+	public static void main(String[] args) 
+	{
+		int intArr[][]= {{10,20,15,22,35}};   //Get the Array
+		
+		System.out.println("Intger Array: "+Arrays.deepHashCode(intArr)); //To get the deep hashcode of the arrays
+		
+
+	}
+
+}
