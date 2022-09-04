1ed04aae74808a5359b07dabc297b7bdd8dbdd33
shwet <shwet@Shweta>
2022-09-04 15:17:08 +0530

    Deep To String

---
+++ TNS_JAVA_CORE BATCH_24/src/com/cg/core/DeepToString.java
@@ -0,0 +1,16 @@
+package com.cg.core;
+
+import java.util.Arrays;
+
+public class DeepToString {
+
+	public static void main(String[] args) 
+	{
+		int intArr[][]= {{40,80,10,42,39}}; //Get the Array
+		
+		System.out.println("Intger Array: " +Arrays.deepToString(intArr));
+		
+
+	}
+
+}
