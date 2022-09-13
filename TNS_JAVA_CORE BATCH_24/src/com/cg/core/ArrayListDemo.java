f8a300460d148166f1a43be95052ff639e4dbaca
shwet <shwet@Shweta>
2022-09-13 18:52:28 +0530

    Array List

---
+++ TNS_JAVA_CORE BATCH_24/src/com/cg/core/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package com.cg.core;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		 ArrayList l=new ArrayList();
+		 l.add("Shweta");
+		 l.add(23);
+		 l.add(50);
+		 l.add("o+");
+		 l.add(null);
+		 
+		 System.out.println(l);
+		 
+		 l.remove(1);
+		 
+		 System.out.println(l);
+		 
+		 l.add(0,"Mrs");
+		 
+		 System.out.println(l);
+		 
+		
+	     
+}
+	}
+
+
